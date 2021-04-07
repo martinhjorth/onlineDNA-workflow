a1fe22c007d3c4c0ec67dd79d0b317fddce230a1
Kasper Skytte Andersen <knaldhat@gmail.com>
2021-04-07 10:27:05 +0200

    remove all conda envs + modules

--- scripts/startWorkflow.sh
+++ scripts/startWorkflow.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#conda install -c conda-forge biopython
 fastqDir=$1
 threadNo="${2:-30}"
 OUT_DIR=$(date '+%Y-%m-%d'_processed)
@@ -8,27 +7,9 @@ version="2.0"
 set -o errexit -o pipefail -o noclobber -o nounset
 [  -z "$fastqDir" ] && { echo "No path to a fastq directory provided. Exiting..."; exit 0; }
 
-
 mkdir -p $OUT_DIR
-# Clean module space
-module purge
 
-# Load Python
-module load Python/3.6.4-foss-2018a --quiet
-
-# Run barcode_compiler
-python scripts/barcode_compiler.py
-
-# Load snakemake environment
-echo "Loading Miniconda"
-module load Miniconda3/4.9.2-foss-2020b --quiet
-
-# echo "Activating snakemake"
-#conda init bash
-set +eu
-. activate /srv/MA/users/mha/CondaEnvs/snakemake
-set -eu
-#conda activate /srv/MA/users/mha/CondaEnvs/snakemake
+python /opt/scripts/barcode_compiler.py
 
 # Select workflow
 title="onlineDNA showcase workflow v. $version"
@@ -37,7 +18,6 @@ options=("bV1-3", "bV4-A")
 echo "$title"
 PS3="$prompt "
 select opt in "${options[@]}" "Quit"; do 
-
     case "$REPLY" in
         1)
             echo ""
@@ -56,8 +36,5 @@ select opt in "${options[@]}" "Quit"; do
   esac
 done
 
-
-
-#conda activate snakemake
 echo "Running your snakemake workflow ($workFlowSelect) with $threadNo threads"
-snakemake --cores 20 --config fastq=$fastqDir threads=$threadNo workflow=$workFlowSelect --use-conda
+snakemake --cores $threadNo --config fastq=$fastqDir threads=$threadNo workflow=$workFlowSelect
